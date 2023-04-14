e62fb91055086063aca30acf65274e02a4ada93f
ReDay Zarra <redayzarra@gmail.com>
2023-04-14 10:37:12 -0400

    Changing background color

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         </GridItem>
       </Show>
 
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         <GameGrid />
       </GridItem>
     </Grid>
