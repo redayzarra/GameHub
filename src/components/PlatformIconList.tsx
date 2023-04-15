e3a61fe9acb81b91f87b9c97d46597946db6a421
ReDay Zarra <redayzarra@gmail.com>
2023-04-15 09:45:52 -0400

    Mapping platform slug to icon

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import { IconType } from "react-icons/";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
 import { Platform } from "../hooks/useGames";
-import { Text } from "@chakra-ui/react";
+import { HStack, Icon, Text } from "@chakra-ui/react";
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
   return (
-    <>
+    <HStack>
       {platforms.map((platform) => (
-        <Text>{platform.name}</Text>
+        <Icon as={iconMap[platform.slug]}></Icon>
       ))}
-    </>
+    </HStack>
   );
 };
 
