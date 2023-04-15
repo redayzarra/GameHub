6671e473ca1d862c1d54b6a521feb42572c4c1e8
ReDay Zarra <redayzarra@gmail.com>
2023-04-15 09:20:52 -0400

    Making the grid layout responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/logo.png";
+import logo from "../assets/YT_logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justify={"space-between"} padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="75px" />
       <ColorModeSwitch />
     </HStack>
   );
