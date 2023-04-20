9929a4284eb5767506dd7062120ee7d367562803
ReDay Zarra <redayzarra@gmail.com>
2023-04-20 08:15:07 -0400

    Building search bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/YT_logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justify={"space-between"} padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="75px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
