cbbce20cdf5dc68fe75481101af7d4fc4ff5b3c4
ReDay Zarra <redayzarra@gmail.com>
2023-04-20 08:36:53 -0400

    Implementing search feature through the search bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/YT_logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({onSearch}: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="75px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch}/>
       <ColorModeSwitch />
     </HStack>
   );
