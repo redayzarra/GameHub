80086602478928855a142f50e8f83a0c6a29f3eb
ReDay Zarra <redayzarra@gmail.com>
2023-04-21 08:32:28 -0400

    Implementing emojis based on rating but not keeping them

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,8 @@
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
-import React from "react";
 import { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
+import Emoji from "./Emoji";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
