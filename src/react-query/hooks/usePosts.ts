3079188da4cd9a27678f6757be2573d7e4a0d32f
ReDay Zarra <redayzarra@gmail.com>
2023-04-25 08:47:38 -0400

    Finishing data fetching exercise

---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const usePosts = () => useQuery<Post[], Error>({
+  queryKey: ["posts"],
+  queryFn: () => axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => (res.data)),
+  staleTime: 60 * 1000 //1 min
+})
+
+export default usePosts
