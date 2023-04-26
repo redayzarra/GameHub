edc887c36905aa35ac9350c484d2eec850799dd4
ReDay Zarra <redayzarra@gmail.com>
2023-04-26 10:57:34 -0400

    Showing adding progress and disabling button during mutation

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -22,6 +22,8 @@ const TodoForm = () => {
         savedTodo,
         ...(todos || []),
       ]);
+
+      if (ref.current) ref.current.value = "";
     },
   });
   const ref = useRef<HTMLInputElement>(null);
@@ -49,7 +51,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Adding..." : "Add"}
+          </button>
         </div>
       </form>
     </>
