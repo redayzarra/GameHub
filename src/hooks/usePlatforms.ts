4b2288eca031b5c910ea9b907cd7fb9b03145454
ReDay Zarra <redayzarra@gmail.com>
2023-04-28 07:03:49 -0400

    Fixing strange bug for platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,8 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import PlatformIconList from "../components/PlatformIconList";
+import PlatformSelector from "../components/PlatformSelector";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 import { Platform } from "./useGames";
 
 interface Props {
@@ -8,4 +12,12 @@ interface Props {
 }
 
 
-export const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+export const usePlatforms = () => useQuery({
+  queryKey: ["platforms"],
+  queryFn: () =>
+   apiClient
+    .get<FetchResponse<Platform>>("/platforms/lists/parents")
+    .then(res => res.data),
+  staleTime: 24 * 3600 * 1000,
+  initialData: { count: platforms.length, results: platforms}
+})
