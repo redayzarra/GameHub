94ffc7458f75bd77eff9087b6290666bc65b8457
ReDay Zarra <redayzarra@gmail.com>
2023-05-01 07:06:08 -0400

    Implementing button for infinite queries

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
 import genres from "../data/genres";
-import APIClient, { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
 
@@ -10,11 +10,12 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => useQuery({
-  queryKey: ["genres"],
-  queryFn: apiClient.getAll,
-  staleTime: 24 * 60 * 60 * 1000,
-  initialData: { count: genres.length, results: genres }
-})
+const useGenres = () =>
+  useQuery({
+    queryKey: ['genres'],
+    queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: genres,
+  });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import platforms from '../data/platforms';
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
@@ -9,9 +10,10 @@ export interface Platform {
   slug: string;
 }
 
-export const usePlatforms = () => useQuery({
-  queryKey: ["platforms"],
-  queryFn: apiClient.getAll,
-  staleTime: 24 * 3600 * 1000,
-  initialData: {count: platforms.length, results: platforms}
-})
+export const usePlatforms = () =>
+  useQuery({
+    queryKey: ['platforms'],
+    queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: platforms,
+  });
