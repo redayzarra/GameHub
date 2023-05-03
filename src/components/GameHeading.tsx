ffb55f2a6980e29dc9eab1df21fb34f6dd9ffe8d
ReDay Zarra <redayzarra@gmail.com>
2023-05-03 06:37:33 -0400

    Adding genre lookup hook

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,16 +1,15 @@
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
-import useGenres from "../hooks/useGenres";
-import { Platform, usePlatforms } from "../hooks/usePlatforms";
+import useGenre from "../hooks/useGenre";
 import usePlatform from "../hooks/usePlatform";
+import { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genres } = useGenres();
-  const genre = genres?.results.find((g) => g.id == gameQuery.genreId);
+  const genre = useGenre(gameQuery.genreId);
 
   const platform = usePlatform(gameQuery.platformId) as Platform | undefined;
 
--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -2,7 +2,7 @@ import { usePlatforms } from "./usePlatforms";
 
 const usePlatform = (id?: number) => {
   const { data: platforms } = usePlatforms();
-  const platform = platforms?.results.find((p) => p.id == id);
+  return platforms?.results.find((p) => p.id == id);
 
 }
 
