bd0ca83c93db769d0296fd156ec44108b2676877
ReDay Zarra <redayzarra@gmail.com>
2023-05-03 06:42:35 -0400

    Resolving the bug the right way

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import APIClient from "../services/api-client";
+import genres from '../data/genres';
 
 const apiClient = new APIClient<Genre>("/genres");
 
@@ -10,12 +11,11 @@ export interface Genre {
 }
 
 const useGenres = () =>
-  useQuery(
-    ["genres"], // queryKey
-    apiClient.getAll, // queryFn
-    {
-      staleTime: 24 * 60 * 60 * 1000, // configuration object
-    }
-  );
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 1000,
+    // initialData: {count: genres.length, results: genres, next: null}
+  });
 
 export default useGenres;
