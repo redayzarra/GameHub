189223edb1535c3f3a2ec90a14b353485b8d648f
ReDay Zarra <redayzarra@gmail.com>
2023-05-07 05:28:59 -0400

    Debugging TaskList - missing parenthesis

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useReducer } from "react";
-import LoginStatus from "./state-management/LoginStatus";
+import HomePage from "./state-management/HomePage";
+import NavBar from "./state-management/NavBar";
+import TasksContext from "./state-management/contexts/tasksContext";
 import tasksReducer from "./state-management/reducers/taskReducer";
 
 function App() {
   const [tasks, dispatch] = useReducer(tasksReducer, []);
 
-  return <><NavBar /> <Homepage /></>
+  return (
+    <>
+      <TasksContext.Provider value={{ tasks, dispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
+    </>
+  );
 }
 
 export default App;
--- src/state-management/TaskList.tsx
+++ src/state-management/TaskList.tsx
@@ -1,39 +1,39 @@
-import { useReducer } from "react";
+import { useContext, useReducer } from "react";
 import tasksReducer from "./reducers/taskReducer";
+import TasksContext from "./contexts/tasksContext";
 
 const TaskList = () => {
+  const { tasks, dispatch } = useContext(TasksContext);
 
-  return (
-    <>
-      <button
-        onClick={() =>
-          dispatch({
-            type: "ADD",
-            task: { id: Date.now(), title: "Task" + Date.now() },
-          })
-        }
-        className="btn btn-primary my-3"
-      >
-        Add Task
-      </button>
-      <ul className="list-group">
-        {tasks.map((task) => (
-          <li
-            key={task.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
+  <>
+    <button
+      onClick={() =>
+        dispatch({
+          type: "ADD",
+          task: { id: Date.now(), title: "Task" + Date.now() },
+        })
+      }
+      className="btn btn-primary my-3"
+    >
+      Add Task
+    </button>
+    <ul className="list-group">
+      {tasks.map((task) => (
+        <li
+          key={task.id}
+          className="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span className="flex-grow-1">{task.title}</span>
+          <button
+            className="btn btn-outline-danger"
+            onClick={() => dispatch({ type: "DELETE", taskId: task.id })}
           >
-            <span className="flex-grow-1">{task.title}</span>
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => dispatch({ type: "DELETE", taskId: task.id })}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  </>;
 };
 
 export default TaskList;
