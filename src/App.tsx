5028af3d6c1cca3fd47a87f65ecaf01871c9476a
ReDay Zarra <redayzarra@gmail.com>
2023-05-09 08:20:32 -0400

    Creating a custom context provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,19 @@ import TasksContext from "./state-management/contexts/tasksContext";
 import tasksReducer from "./state-management/reducers/taskReducer";
 import loginReducer from "./state-management/reducers/loginReducer";
 import AuthContext from "./state-management/contexts/authContext";
+import AuthProvider from "./state-management/AuthProvider";
 
 function App() {
   const [tasks, tasksdispatch] = useReducer(tasksReducer, []);
-  const [user, authDispatch] = useReducer(loginReducer, "");
 
   return (
     <>
-      <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+      <AuthProvider>
         <TasksContext.Provider value={{ tasks, dispatch: tasksdispatch }}>
           <NavBar />
           <HomePage />
         </TasksContext.Provider>
-      </AuthContext.Provider>
+      </AuthProvider>
     </>
   );
 }
