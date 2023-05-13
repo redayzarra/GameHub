47e2e7795a3dd8261bc92aec2fa9b701ce41885b
ReDay Zarra <redayzarra@gmail.com>
2023-05-13 06:33:38 -0400

    Sharing state between two components with Zustand - very concise!

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,13 +1,15 @@
 import { useContext } from "react";
 import LoginStatus from "./auth/LoginStatus";
 import TasksContext from "./tasks/tasksContext";
+import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks, dispatch } = useContext(TasksContext);
+  const { counter } = useCounterStore();
 
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">{counter}</span>
       <LoginStatus />
     </nav>
   );
