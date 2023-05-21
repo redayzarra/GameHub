a500ac0cb7fcb21d952a5bd17b5e2f036cee2273
ReDay Zarra <redayzarra@gmail.com>
2023-05-20 23:48:30 -0400

    Successfully implementing navigation

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserList from "./UserList";
+import ContactPage from "./ContactPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/users", element: <UserList /> },
-  { path: "/users", element: <UserList /> },
+  { path: "/contact", element: <ContactPage /> },
 ]);
 
 export default router;
