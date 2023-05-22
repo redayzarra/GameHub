6cf8cf9a0a6608c8aa80b0396f9802da6fb1c150
ReDay Zarra <redayzarra@gmail.com>
2023-05-22 10:49:54 -0400

    Getting data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,14 @@
+import { useLocation, useParams } from "react-router";
+import { useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const UserDetailPage = () => {
+    const params = useParams();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const location = useLocation();
+    console.log(searchParams.get("name"));
+  };
+
   return <p>User</p>;
 };
 
