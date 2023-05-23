dc767c92f562a19ef4a85f4fe1ee50578261e02f
ReDay Zarra <redayzarra@gmail.com>
2023-05-23 10:55:10 -0400

    Creating nested routes and fully funcitonal navigation bar

--- src/routing/Layout.tsx
+++ src/routing/Layout.tsx
@@ -1,10 +1,13 @@
+import { Outlet } from "react-router";
 import NavBar from "./NavBar";
 
 const Layout = () => {
   return (
     <>
       <NavBar />
-      <div id="main"></div>
+      <div id="main">
+        <Outlet />
+      </div>
     </>
   );
 };
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,11 +3,18 @@ import HomePage from "./HomePage";
 import UserList from "./UserList";
 import ContactPage from "./ContactPage";
 import UserDetailPage from "./UserDetailPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/users", element: <UserList /> },
-  { path: "/users/:id", element: <UserDetailPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "users", element: <UserList /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+    ],
+  },
 ]);
 
 export default router;
