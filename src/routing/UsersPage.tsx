a1ca3b6bee88233555fca523259611042dfe9566
ReDay Zarra <redayzarra@gmail.com>
2023-05-23 19:22:47 -0400

    Adding UsersPage with user details

---
+++ src/routing/UsersPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UserListPage from "./UserList";
+
+const UsersPage = () => {
+  return (
+    <div className="row">
+      <div className="col">
+        <UserListPage />
+      </div>
+      <div className="col"></div>
+    </div>
+  );
+};
+
+export default UsersPage;
