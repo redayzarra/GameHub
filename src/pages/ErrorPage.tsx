3ecf589fbef2ab53db45ae57f278ac476bc01bd3
ReDay Zarra <redayzarra@gmail.com>
2023-05-27 17:42:20 -0400

    Adding Error page and displaying error message

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,17 +1,21 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const ErrorPage = () => {
   const error = useRouteError();
 
   return (
     <>
-      <Heading>Oops</Heading>
-      <Text>
-        {isRouteErrorResponse(error)
-          ? "This page does not exist"
-          : "An unexpected error occurred."}
-      </Text>
+      <NavBar />
+      <Box padding={5}>
+        <Heading>Oops</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "This page does not exist"
+            : "An unexpected error occurred."}
+        </Text>
+      </Box>
     </>
   );
 };
