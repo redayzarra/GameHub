b67250d5c5583a1d6cb71f755282e31e976811f9
ReDay Zarra <redayzarra@gmail.com>
2023-05-30 22:34:52 -0400

    Adding all definition items to simple grid

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpendableText from "../components/ExpendableText";
 import DefinitionItem from "../components/DefinitionItem";
 import CriticScore from "../components/CriticScore";
@@ -17,28 +17,29 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpendableText>{game.description_raw}</ExpendableText>
-      
-      <DefinitionItem term="Platforms">
-        {game.parent_platforms?.map(({ platform }) => (
-          <Text key={platform.id}>{platform.name}</Text>
-        ))}
-      </DefinitionItem>
+      <SimpleGrid columns={2} as="dl">
+        <DefinitionItem term="Platforms">
+          {game.parent_platforms?.map(({ platform }) => (
+            <Text key={platform.id}>{platform.name}</Text>
+          ))}
+        </DefinitionItem>
 
-      <DefinitionItem term="Metascore">
-        <CriticScore score={game.metacritic} />
-      </DefinitionItem>
-      
-      <DefinitionItem term="Genres">
-        {game.genres.map((genre) => (
-          <Text key={genre.id}>{genre.name}</Text>
-        ))}
-      </DefinitionItem>
+        <DefinitionItem term="Metascore">
+          <CriticScore score={game.metacritic} />
+        </DefinitionItem>
 
-      <DefinitionItem term="Publishers">
-        {game.publishers?.map((publisher) => (
-          <Text key={publisher.id}>{publisher.name}</Text>
-        ))}
-      </DefinitionItem>
+        <DefinitionItem term="Genres">
+          {game.genres.map((genre) => (
+            <Text key={genre.id}>{genre.name}</Text>
+          ))}
+        </DefinitionItem>
+
+        <DefinitionItem term="Publishers">
+          {game.publishers?.map((publisher) => (
+            <Text key={publisher.id}>{publisher.name}</Text>
+          ))}
+        </DefinitionItem>
+      </SimpleGrid>
     </>
   );
 };
