f37bc13e7daffa517a37303b92f7d174225ba711
ReDay Zarra <redayzarra@gmail.com>
2023-05-30 22:39:14 -0400

    Separating game attributes to separate component

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,8 @@
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpendableText from "../components/ExpendableText";
-import DefinitionItem from "../components/DefinitionItem";
-import CriticScore from "../components/CriticScore";
+import useGame from "../hooks/useGame";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -17,29 +16,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpendableText>{game.description_raw}</ExpendableText>
-      <SimpleGrid columns={2} as="dl">
-        <DefinitionItem term="Platforms">
-          {game.parent_platforms?.map(({ platform }) => (
-            <Text key={platform.id}>{platform.name}</Text>
-          ))}
-        </DefinitionItem>
-
-        <DefinitionItem term="Metascore">
-          <CriticScore score={game.metacritic} />
-        </DefinitionItem>
-
-        <DefinitionItem term="Genres">
-          {game.genres.map((genre) => (
-            <Text key={genre.id}>{genre.name}</Text>
-          ))}
-        </DefinitionItem>
-
-        <DefinitionItem term="Publishers">
-          {game.publishers?.map((publisher) => (
-            <Text key={publisher.id}>{publisher.name}</Text>
-          ))}
-        </DefinitionItem>
-      </SimpleGrid>
+      <GameAttributes game={game} />
     </>
   );
 };
